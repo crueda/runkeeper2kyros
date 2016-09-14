1d4aefd421c30fb5e569492598c332e2f90e00d4
Carlos Rueda Morales <crueda@gmail.com>
2016-09-14 14:16:21 +0200

    asdfasdfasdfasdfasdfasdfadsf

--- runkeeper2kyros.py
+++ runkeeper2kyros.py
@@ -50,6 +50,14 @@ RUNKEEPER_ACCEPT_ACTIVITY = config['accept_activity']
 DEFAULT_SLEEP_TIME = float(config['sleep_time'])
 KCS_SLEEP_TIME = float(config['kcs_sleep_time'])
 
+latitudeDict = {}
+longitudeDict = {}
+speedDict = {}
+altitudeDict = {}
+distanceDict = {}
+hrDict = {}
+posDateDict = {}
+
 ########################################################################
 
 # Se definen los logs internos que usaremos para comprobar errores
@@ -75,43 +83,47 @@ except:
 #
 ########################################################################
 
-def send2kcs(body):
+def send2kcs(imei):
+	global latitudeDict, longitudeDict, speedDict, altitudeDict, distanceDict, hrDict, posDateDict
 	connectionRetry = 0.5
-	try:
-		socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socketKCS.connect((KCS_HOST, int(KCS_PORT)))
-		connectedKCS = True
-		socketKCS.send(body + '\r\n')
-		logger.debug ("Sent to KCS: %s " % body)
-		sendMessage = True
-		socketKCS.close()
-		time.sleep(KCS_SLEEP_TIME)
-	except socket.error,v:
-		logger.error('Error sending data: %s', v[0])
+	for timestamp in latitudeDict.keys():
+		trama_kcs = str(imei) + ',' + str(posDateDict[timestamp]) + ',' + str(longitudeDict[timestamp]) + ',' + str(latitudeDict[timestamp]) + ',' + str(speedDict[timestamp]) + ',' + str(hrDict[timestamp]) + ',' + str(altitudeDict[timestamp]) + ',9,2,0.0,0.9,3836'
+
 		try:
+			socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			socketKCS.connect((KCS_HOST, int(KCS_PORT)))
+			connectedKCS = True
+			socketKCS.send(trama_kcs + '\r\n')
+			logger.debug ("Sent to KCS: %s " % trama_kcs)
+			sendMessage = True
 			socketKCS.close()
-			logger.info('Trying close connection...')
-		except Exception, error:
-			logger.info('Error closing connection: %s', error)
-			pass
-			while sendMessage==False:
-				try:
-					logger.info('Trying reconnection to KCS...')
-					socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					socketKCS.connect((KCS_HOST, int(KCS_PORT)))
-					connectedKCS = True
-					socketKCS.send(body + '\r\n')
-					logger.info ("Sent to KCS: %s " % body)
-					sendMessage = True
-					socketKCS.close()
-				except Exception, error:
-					logger.info('Reconnection to KCS failed....waiting %d seconds to retry.' , connectionRetry)
-					sendMessage=False
+			time.sleep(KCS_SLEEP_TIME)
+		except socket.error,v:
+			logger.error('Error sending data: %s', v[0])
+			try:
+				socketKCS.close()
+				logger.info('Trying close connection...')
+			except Exception, error:
+				logger.info('Error closing connection: %s', error)
+				pass
+				while sendMessage==False:
 					try:
+						logger.info('Trying reconnection to KCS...')
+						socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+						socketKCS.connect((KCS_HOST, int(KCS_PORT)))
+						connectedKCS = True
+						socketKCS.send(trama_kcs + '\r\n')
+						logger.info ("Sent to KCS: %s " % trama_kcs)
+						sendMessage = True
 						socketKCS.close()
-					except:
-						pass
-					time.sleep(connectionRetry)
+					except Exception, error:
+						logger.info('Reconnection to KCS failed....waiting %d seconds to retry.' , connectionRetry)
+						sendMessage=False
+						try:
+							socketKCS.close()
+						except:
+							pass
+						time.sleep(connectionRetry)
 
 def getRunkeeperKyrosData():
 	try:
@@ -162,6 +174,9 @@ def updateLastActivityId(deviceId, activityId):
 
 
 def processActivity(authorization, imei, activityId):
+	global latitudeDict, longitudeDict, speedDict, altitudeDict, distanceDict, hrDict, posDateDict
+	latitudeDict, longitudeDict, speedDict, altitudeDict, distanceDict, hrDict, posDateDict = {},{},{},{},{},{}, {}
+	
 	logger.info ("process runkeeper activity: %s " % str(activityId))
 	headers = {"Content-type": "application/x-www-form-urlencoded", "Host": "api.runkeeper.com", "Accept": RUNKEEPER_ACCEPT_ACTIVITY, "Authorization": "Bearer " + authorization}	
 	try:
@@ -171,10 +186,8 @@ def processActivity(authorization, imei, activityId):
 			activity = json.loads(response.content)
 			str_start_time = activity['start_time']
 			start_time = (time.mktime(time.strptime(str_start_time, '%a, %d %b %Y %H:%M:%S'))*1000)
-			#print start_time
 			activity_path = activity['path']
-			activity_distance = activity['distance']
-			#lon_anterior, lat_anterior, epoch_anterior = 0,0,0
+			activity_distance = activity['distance']			
 			timestamp_anterior, metros_anterior = 0,0
 			for index in range(len(activity_path)):
 				altitude = int(activity_path[index]['altitude'])
@@ -182,36 +195,44 @@ def processActivity(authorization, imei, activityId):
 				longitude = activity_path[index]['longitude']
 				timestamp = activity_path[index]['timestamp']
 				epoch_date = start_time + (int(timestamp*1000))	
-				#print epoch_date			
 				speed = 0
 				if (timestamp_anterior!=0):
 					metros = activity_distance[index]['distance']
 					distancia = metros - metros_anterior
 					segundos = timestamp - timestamp_anterior
 					speed = (distancia/segundos)*3.6
-					#print speed
 					metros_anterior = metros
 
-				'''
-				if (lon_anterior!=0):
-					segundos = (epoch_date - epoch_anterior)/1000
-					pos_anterior = (lat_anterior, lon_anterior)
-					pos_actual = (latitude, longitude)
-					metros = haversine(pos_anterior, pos_actual) * 1000
-					velocidad = (metros/segundos)*3.6
-					#print velocidad
-				'''
 				timestamp_anterior = timestamp
-				heading = 0
-				#lon_anterior = longitude
-				#lat_anterior = latitude
-				#epoch_anterior = epoch_date
 
-				#trama_gprmd= GPRMC,113548.000,A,4020.1086,N,00340.2196,W,44.39,257.57,130916,,
 				s = epoch_date / 1000.0
 				pos_date = datetime.datetime.fromtimestamp(s).strftime('%Y%m%d%H%M%S')
-				trama_kcs = str(imei) + ',' + str(pos_date) + ',' + str(longitude) + ',' + str(latitude) + ',' + str(speed) + ',' + str(heading) + ',' + str(altitude) + ',9,2,0.0,0.9,3836'
-				send2kcs (trama_kcs)
+				
+				# guardar los datos en diccionario
+				longitudeDict[epoch_date] = longitude
+				latitudeDict[epoch_date] = latitude
+				speedDict[epoch_date] = speed
+				altitudeDict[epoch_date] = altitude
+				hrDict[epoch_date] = 0
+				posDateDict[epoch_date] = pos_date
+				#print epoch_date
+
+			# procesar HR
+			activity_hr = activity['heart_rate']
+			for index in range(len(activity_hr)):
+				heart_rate = activity_hr[index]['heart_rate']
+				timestamp_hr = activity_hr[index]['timestamp']
+				epoch_timestamp_hr = start_time + (int(timestamp_hr*1000))
+				encontrado = False
+				for k in sorted(hrDict.iterkeys()):
+					if (k>epoch_timestamp_hr and encontrado==False):
+						encontrado = True
+						hrDict[k] = heart_rate
+
+			send2kcs(imei)
+				#trama_gprmc= GPRMC,113548.000,A,4020.1086,N,00340.2196,W,44.39,257.57,130916,,
+				#trama_kcs = str(imei) + ',' + str(pos_date) + ',' + str(longitude) + ',' + str(latitude) + ',' + str(speed) + ',0,' + str(altitude) + ',9,2,0.0,0.9,3836'
+				#send2kcs (trama_kcs)
 			return True
 		else:
 			logger.debug("Codigo de error al recuperar los datos de la actividad: " + str(response.status_code))
@@ -270,8 +291,8 @@ def main():
 		
 
 if __name__ == '__main__':
-    main()
-    #processActivity(RUNKEEPER_AUTHORIZATION, 109997775551, 862297072)
+    #main()
+    processActivity(RUNKEEPER_AUTHORIZATION, 109997775552, 863307865)
     
     
     
